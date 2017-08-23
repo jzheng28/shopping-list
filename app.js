aee5fa18943c4f51ebdc9b073a697fd631ca4b33
jzheng28 <zheng.jeannie@gmail.com>
2017-08-22 23:01:25 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
-/*
+$(document).ready(function(){
 
-NOTES:
-1. Not sure how inputVal works with addItem --> how to get the value of the input,
-put it in the array, and spit out the array. Should the inputVal function
-be a paremeter in the addItem function?
+ // add item 
+  	$('#js-shopping-list-form').submit(function(e){
+			e.preventDefault();
+			//get value of input
+				var item = $('#shopping-list-entry').val();
+        var ul = $(".shopping-list");
+         var newHtml=listTemplate1+item+listTemplate2;
+         //append html template to ul
+         ul.append(newHtml);
+         document.getElementById("js-shopping-list-form").reset();
+   });
+   
+ 
+ /* Cross off functionality
+Use jQuery event delegation to attach event listener to static node, which will fire for descendents that match the specified selector
+ 
+ */
+    $(".shopping-list").on('click', '.shopping-item-toggle',function(e) {
+    e.stopPropagation();
+  
+    if ($(this).closest('li').hasClass('shopping-item__checked')){
+    	$(this).closest('li').removeClass('shopping-item__checked')
+    } else{
+    $(this).closest('li').addClass('shopping-item__checked');
+    }
+  });
+  
+  // Delete functionality
+     $(".shopping-list").on('click', '.shopping-item-delete',function(e) {
+    e.stopPropagation();
+     $(this).closest("li").remove();
+ 
+  });
+   
+})
 
 
-*/
-
-
-//shopping list array
-var shoppingList = [];
-
-/*get value of input
-function inputVal(){
-	var input = $('#shopping-list-entry').val();
-	return input;
-	console.log(input);
-
-}
-*/
-
-//add item to shopping list
-
-function addItem(item){
-	var item = $('#shopping-list-entry').val();
-	shoppingList.push({
-
-		name: item,
-		checked: false,
-
-		});
-	return item;
-
-}
-
-
-//list template to inject into the html
-
-var listTemplate = (
+// HTML template for shopping list item
+var listTemplate1 = (
 	 
      '<li>' +
-        '<span class="shopping-item"></span>'+
+        '<span class="shopping-item">');
+var listTemplate2=(        
+        '</span>'+
        ' <div class="shopping-item-controls">'+
          ' <button class="shopping-item-toggle">'+
             '<span class="button-label">check</span>'+
@@ -53,48 +55,6 @@ var listTemplate = (
       '</li>'    
 
 )
-
-//renders the list template 
-// ??
-function renderTemplate(){
-
-}
-
-//handle form submission
-
-function formSubmission(){
-	$('#js-shopping-list-form').submit(function(e){
-		e.preventDefault();
-		//Display text input here
-		$('.shopping-item').text(addItem());
-		
-	});
-}
-
-//run the form submission
-
-$(function() {
-	formSubmission();
-});
-
-
-
-/* TESTING
-
-function test(){
-	$('#js-shopping-list-form').submit(function(e){
-		e.preventDefault();
-		var message = "this is amazing";
-		alert(message);
-	});
-}
-
-
-$(function(){
-	test();
-});
-
-*/
 
 
 
